c724c68bcb738c0b3b0ebf17fdb9d93642c16303
Patrick <ruomungai@gmail.com>
2022-08-16 09:11:43 +0300

    commit tree 44a4a72f1edc2f94d9b322a81d78692ec5598353

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
 def countdown
+    number=1
+    while number <10
+        puts "#{number} SECOND(S)!\n"
+        number+=1
+    end
+    puts "HAPPY NEW YEAR!"
 end
+countdown 
